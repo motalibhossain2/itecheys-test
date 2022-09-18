9d3ed02004e57fc98284dde483965ff9fc4870c8
motalibhossain2 <motalibhossainrbk34@gmail.com>
2022-09-18 17:25:39 +0600

    update
    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,107 +1,148 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Contact = () => {
-  const [name, setName] = useState({
-    fname: "",
-    lname: "",
-    company: "",
-    phone: "",
-    email: "",
-    url: "",
-  })
+  const [category, setCategory] = useState([]);
+  const [formData, SetFormData] = useState({
+    name: "",
+    // category: "",
+    // slug: "",
+    // description: "",
+    // is_active: "",
+  });
+  const [name, setName] = useState("");
 
-  const handleOnChange = (e) => {
-    const name = e.target.name
-    const value = e.target.value
-    setName((prev) => {
-      return { ...prev, [name]: value }
-    })
-  }
+  // ================================================
+  //          Category data fecth from Api
+  // ===============================================
+  const fetchBlogs = async () => {
+    const response = await fetch("https://motalib.pythonanywhere.com/api/blog-category/");
+    // const response = await fetch("http://127.0.0.1:8000/api/blog-category/");
+    const data = await response.json();
+    return data;
+  };
+
+  useEffect(() => {
+    fetchBlogs().then((data) => {
+      setCategory(data);
+    });
+  }, []);
+  console.log("category", category);
+  // ================================================
+  //          /From data save
+  // ===============================================
+  const HandelChange = (e) => {
+    const { name, value } = e.target;
+    // SetFormData((Prev) => {
+    //   return { ...Prev, [name]: value };
+    // });
+    setName((Prev) => {
+      return { ...Prev, [name]: value };
+    });
+  };
   const HandleSubmit = (e) => {
     e.preventDefault()
-    const datapost = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts/", {
+    const form_data = new FormData();
+    form_data.append("name", name);
+    const datapost = () => {
+      const res = fetch("https://motalib.pythonanywhere.com/api/blog-category", {
         method: "post",
         headers: {
-          // "Content-Type": "multipart/form-data",
+          "Content-Type": "multipart/form-data",
         },
-        body: name})
-        return res
+        body: form_data
+      })
+      return res
     }
-    datapost().then((res)=>{
+    datapost().then((res) => {
       console.log(res)
-      if(res.ok){
+      if (res.ok) {
         alert("successfull")
       }
-      else{
+      else {
         console.log("error");
       }
     })
-    
+
     e.target.reset()
   }
-  console.log(name);
+  console.log(formData);
 
 
   return (
     <>
       <div className="container">
-        <form onSubmit={HandleSubmit}>
-          <h2>Contact Us</h2>
+        <form className="submit-post" onSubmit={HandleSubmit}>
+          <h2>Submit your Article</h2>
           <div className="row">
             <div className="col-md-6">
               <div className="form-group">
-                <label >First Name</label>
-                <input type="text" name='fname' className="form-control" placeholder="" id="first" onChange={handleOnChange} />
+                <label> Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  placeholder=""
+                  onChange={HandelChange}
+                />
               </div>
             </div>
 
-            <div className="col-md-6">
-              <div className="form-group">
-                <label >Last Name</label>
-                <input type="text" name='lname' className="form-control" placeholder="" id="last" onChange={handleOnChange} />
-              </div>
-            </div>
+            {/* <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Catagory</label>
+                                <select className="form-select" name="category" onChange={HandelChange}>
+                                    <option>Catagory</option>
+                                    {category.map((Item, index) => {
+                                        return (
+                                            <option value= {Item.id} key={index}>
+                                                {Item.name}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        </div> */}
           </div>
-
+          {/* <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Description</label>
+                                <textarea
+                                    type="text"
+                                    name="description"
+                                    className="form-control"
+                                    id="description"
+                                    placeholder="Description"
+                                    onChange={HandelChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Slug</label>
+                                <input
+                                    type="text"
+                                    name="slug"
+                                    className="form-control"
+                                    placeholder=""
+                                    onChange={HandelChange}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="checkbox py-3">
+                        <label>
+                            <input type="checkbox" name="is_active" value="1" id="newsletter" onChange={HandelChange} />{" "}
+                            Is Active!
+                        </label>
+                    </div> */}
           <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label >Company</label>
-                <input type="text" name='company' className="form-control" placeholder="" id="company" onChange={handleOnChange} />
-              </div>
-            </div>
-
-            <div className="col-md-6">
-              <div className="form-group">
-                <label >Phone Number</label>
-                <input type="tel" name='phone' className="form-control" id="phone" placeholder="phone" onChange={handleOnChange} />
-              </div>
+            <div className="col-lg-12">
+              <button type="submit" className="btn btn-primary mt-3">
+                Submit
+              </button>
             </div>
           </div>
-
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label >Email address</label>
-                <input type="email" name='email' className="form-control" id="email" placeholder="email" onChange={handleOnChange} />
-              </div>
-            </div>
-
-            <div className="col-md-6">
-              <div className="form-group">
-                <label >Your Website <small>Please include http://</small></label>
-                <input type="url" name='url' className="form-control" id="url" placeholder="url" onChange={handleOnChange} />
-              </div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="form-group mt-3">
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </div>
-          </div>
-
         </form>
       </div>
     </>
